cb6a6cf67ebb5bf4e4abcfda00156b2476297b03
Tyoma-Pi <artyom.pischulin@gmail.com>
2025-12-18 14:54:30 +0500

    Мой первый проект

---
+++ MyRepoApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MyRepoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int a = 6;
+            a += 4;
+            Console.WriteLine(a);
+        }
+    }
+}
